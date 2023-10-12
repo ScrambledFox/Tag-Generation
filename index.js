fe3ca0397e81de44a3fd113b12ef6bb179c5efb2
Joris Lodewijks <jorislodewijks@3dbyflow.com>
2023-10-12 09:09:56 +0200

    chore: rename

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const getAndWriteAllEvents = require("./getEventsFromAPI");
+const getAndWriteAllEventsFromAPI = require("./getEventsFromAPI");
 const generateTags = require("./generateTags");
 const convertToCSV = require("./toDataframe");
 
 async function Main() {
   console.log("Getting events from API...");
-  await getAndWriteAllEvents();
+  await getAndWriteAllEventsFromAPI();
   await new Promise((resolve) => setTimeout(resolve, 1000));
+
   console.log("Generating tags...");
   await generateTags();
   await new Promise((resolve) => setTimeout(resolve, 1000));
+
   console.log("Converting to CSV...");
   await convertToCSV();
   console.log("Done!");
